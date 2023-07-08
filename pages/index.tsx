494a9532f3bb8326d9aa5b8cc6c141ff7c7053b3
keyvan <key7an@keyvans-MacBook-Air.local>
2023-07-09 00:37:51 +0330

    adding sessions and related stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,33 @@
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+}
+
 export default function Home() {
+  const { data: user } = useCurrentUser();
+
   return (
     <>
-      <h1 className="text-green-500">Netflix</h1>
+      <h1 className="text-green-500 text-4xl">Netflix</h1>
+      <p className=" text-red-500">logged in as {user?.email}</p>
+      <button className="h-10 w-full bg-white " onClick={() => signOut()}>
+        Logout
+      </button>
     </>
   );
 }
