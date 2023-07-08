cdcda24f7be4697d94abe8338ac76bef5d619d68
keyvan <key7an@keyvans-MacBook-Air.local>
2023-07-08 14:25:57 +0330

    adding github authentication

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,7 +1,14 @@
 import Input from "@/components/Input";
 import React, { useCallback, useState } from "react";
+import axios from "axios";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 const Auth = () => {
+  const router = useRouter();
+
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
@@ -14,6 +21,35 @@ const Auth = () => {
     );
   }, []);
 
+  const login = useCallback(async () => {
+    try {
+      await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: "/",
+      });
+
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, password, router]);
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post("/api/register", {
+        email,
+        password,
+        name,
+      });
+
+      login();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, name, password, login]);
+
   return (
     <div
       className="relative h-full w-full bg-[url('/images/hero.jpg')]
@@ -25,7 +61,7 @@ const Auth = () => {
         </nav>
         <div className="flex justify-center">
           <div
-            className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2
+            className="bg-black bg-opacity-50 px-16 py-16 self-center mt-2
             lg:w-2/5 lg:max-w-md rounded-md w-full"
           >
             <h2 className="text-white text-4xl mb-8 font-semibold">
@@ -56,11 +92,33 @@ const Auth = () => {
               />
             </div>
             <button
+              onClick={variant === "login" ? login : register}
               className="bg-red-600 py-3 text-white
-             rounded-md w-full mt-10 hover:bg-red-700"
+              rounded-md w-full mt-10 hover:bg-red-700"
             >
               {variant === "login" ? "Login" : "Sign up"}
             </button>
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div
+                // onClick={() => signIn("google", { callbackUrl: "/" })}
+                onClick={() =>
+                  alert(
+                    "Google authentication is not responding at the moment!"
+                  )
+                }
+                className="w-10 h-10 bg-white rounded-full flex items-center
+               justify-center cursor-pointer hover:opacity-80 transition"
+              >
+                <FcGoogle />
+              </div>
+              <div
+                onClick={() => signIn("github", { callbackUrl: "/" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center
+               justify-center cursor-pointer hover:opacity-80 transition"
+              >
+                <FaGithub />
+              </div>
+            </div>
             <p className="text-neutral-500 mt-12">
               {variant === "login"
                 ? "First time using Netflix? "
