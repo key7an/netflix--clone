e6f228bb72af9356f147d042bfd9bf5dcd8ecfe0
keyvan <key7an@keyvans-MacBook-Air.local>
2023-07-17 16:25:36 +0330

    adding movielist

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -25,22 +25,19 @@ const Billboard = () => {
           {data?.title}
         </p>
         <p
-          className="text-white sm:text-[12px] mt-3
+          className="text-white text-[10px] mt-3
         md:text-lg md:mt-8 w-[90%] lg:w-[50%] md:w-[80%]
         drop-shadow-xl lg:text-2xl opacity-70
         "
         >
           {data?.description}
         </p>
-        <div
-          className="flex flex-row items-center md:mt-4
-        gap-3"
-        >
+        <div className="flex flex-row items-center md:mt-4 gap-3">
           <button
             className="bg-white text-white bg-opacity-30
           rounded-md py-1 md:py-2 px-2 md:px-4 w-auto text-xs
           lg:text-lg font-semibold flex flex-row items-center
-          hover:bg-opacity-10 transition
+          hover:bg-opacity-10 transition mt-4
           "
           >
             <AiOutlineInfoCircle className="mr-1" />
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import { getSession, signOut } from "next-auth/react";
 
 import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import useMovieList from "@/hooks/useMovieList";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -22,10 +24,15 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
+  const { data: movies = [] } = useMovieList();
+
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className="pb-40">
+        <MovieList data={movies} title="Trending Now" />
+      </div>
     </>
   );
 }
